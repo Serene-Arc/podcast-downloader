6e31922da61287bf16aacd26b7a9c3388c4dee82
serene-arc <serenical@gmail.com>
2020-06-17 16:03:58 +1000

    Add fix for weird error when the RSS fails and an error is returned which makes the feed unpickleable

--- podcastdownloader/feed.py
+++ podcastdownloader/feed.py
@@ -14,16 +14,24 @@ class Feed:
         self.feed_episodes = []
         self.downloaded_episodes = []
 
-    def getFeed(self):
+    def __download_rss(self):
         try:
-            rss_feed = requests.get(self.url, timeout=120)
+            self.feed = requests.get(self.url, timeout=120).text
         except requests.exceptions.Timeout:
             print('Failed to get feed at {}'.format(self.url))
+            return
 
-        self.feed = feedparser.parse(rss_feed.text)
+    def getFeed(self):
+        self.__download_rss()
+        self.feed = feedparser.parse(self.feed)
         self.title = self.feed['feed']['title'].encode('utf-8').decode('ascii', 'ignore')
         for entry in self.feed['entries']:
             self.feed_episodes.append(Episode(entry, self.title))
+
+        # if there's an exception in the feed from feedparser, then the entire
+        # object becomes unpicklable and wont work with multiprocessing. it still
+        # seems to get the episodes most of the time so easier to wipe it
+        self.feed = None
 
     def fillEpisodes(self):
         for episode in self.feed_episodes:
