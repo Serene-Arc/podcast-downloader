d83aae5e9b7380fbe0bbe7438fca27cd1c6f0432
serene-arc <serenical@gmail.com>
2020-06-23 19:29:15 +1000

    Fix bug where failed downloads crash audacious writer

--- podcastdownloader/writer.py
+++ podcastdownloader/writer.py
@@ -7,8 +7,11 @@ def writeEpisodeAudacious(feed):
     with open(pathlib.Path(feed.directory, 'episode_playlist.audpl'), 'w') as file:
         file.write('title={}\n'.format(feed.title).replace(' ', '%20'))
         for episode in reversed(feed.feed_episodes):
-            file.write('uri=file://{}\n'.format(episode.path).replace(' ', '%20'))
-            file.write('title={}\n'.format(episode.title).replace(' ', '%20'))
+            try:
+                file.write('uri=file://{}\n'.format(episode.path).replace(' ', '%20'))
+                file.write('title={}\n'.format(episode.title).replace(' ', '%20'))
+            except AttributeError as e:
+                pass
 
 
 def writeEpisodeText(feed):
