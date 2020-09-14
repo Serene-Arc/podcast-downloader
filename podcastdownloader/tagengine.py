76fd456071d0cc075ae8e06ab445e4ed90c2202c
serene-arc <serenical@gmail.com>
2020-09-14 18:07:48 +1000

    Add catch for when an m4a file is mislabeled

--- podcastdownloader/tagengine.py
+++ podcastdownloader/tagengine.py
@@ -67,7 +67,10 @@ def _writeID3Tags(episode: Episode):
 
 
 def _writeMP4Tags(episode: Episode):
-    episode_tags = mutagen.mp4.MP4(episode.path)
+    try:
+        episode_tags = mutagen.mp4.MP4(episode.path)
+    except mutagen.mp4.MP4StreamInfoError:
+        logger.error('Thought {} was an MP4 file but it was not'.format(episode.path.name))
 
     try:
         episode_tags.add_tags()
