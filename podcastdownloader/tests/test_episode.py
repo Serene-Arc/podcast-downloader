4a9e748c16577a889c1f62017dbdc4ba735cf7f8
Serene-Arc <serenical@gmail.com>
2021-04-15 15:57:37 +1000

    Fix formatting

--- podcastdownloader/tests/test_episode.py
+++ podcastdownloader/tests/test_episode.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+
 import pytest
 
 from podcastdownloader.episode import Episode, EpisodeStatus
@@ -11,17 +13,21 @@ def episode():
     feed_dict = {
         'title': 'Bad /Test\0 Title',
         'links': [
-            {'href': 'https://dts.podtrac.com/redirect.mp3/chtbl.com/track/9EE2G/pdst.fm/e/rss.art19.com/episodes/b58e6644-0b5d-492b-ad4a-a773bc701b81.mp3',
+            {
+                'href': 'https://dts.podtrac.com/redirect.mp3/chtbl.com/track/9EE2G/pdst.fm/e/'
+                        'rss.art19.com/episodes/b58e6644-0b5d-492b-ad4a-a773bc701b81.mp3',
                 'length': '4690337',
                 'rel': 'enclosure',
-                'type': 'audio/mpeg'}]}
+                'type': 'audio/mpeg',
+            }]}
     return Episode(feed_dict, 'test podcast')
 
 
 def test_parse_RSS(episode: Episode):
     episode.parse_rss_entry()
     assert episode.title == 'Bad Test Title'
-    assert episode.download_link == 'https://dts.podtrac.com/redirect.mp3/chtbl.com/track/9EE2G/pdst.fm/e/rss.art19.com/episodes/b58e6644-0b5d-492b-ad4a-a773bc701b81.mp3'
+    assert episode.download_link == 'https://dts.podtrac.com/redirect.mp3/chtbl.com/track/9EE2G/pdst.fm/e/' \
+                                    'rss.art19.com/episodes/b58e6644-0b5d-492b-ad4a-a773bc701b81.mp3'
     assert episode.file_type == 'audio/mpeg'
     assert episode.status == EpisodeStatus.PENDING
 
@@ -29,29 +35,33 @@ def test_parse_RSS(episode: Episode):
 def test_path_calc(episode: Episode):
     episode.title = 'test title'
     episode.file_type = 'audio/mpeg'
-    episode.calculate_file_path('testdirectory')
+    episode.calculate_file_path(Path('testdirectory'))
     assert str(episode.path) == 'testdirectory/test podcast/test title.mp3'
 
 
-@pytest.mark.parametrize(('test_value', 'expected'), (('audio/mp4', '.m4a'),
-                                                      ('audio/mp3', '.mp3'),
-                                                      ('audio/mpeg', '.mp3'),
-                                                      ('audio/flac', '.flac'),
-                                                      ('audio/aac', '.aac'),
-                                                      ('audio/ogg', '.opus'),
-                                                      ('audio/mpeg4-generic', '.aac')))
+@pytest.mark.parametrize(('test_value', 'expected'), (
+    ('audio/mp4', '.m4a'),
+    ('audio/mp3', '.mp3'),
+    ('audio/mpeg', '.mp3'),
+    ('audio/flac', '.flac'),
+    ('audio/aac', '.aac'),
+    ('audio/ogg', '.opus'),
+    ('audio/mpeg4-generic', '.aac'),
+))
 def test_path_suffix(episode: Episode, test_value: str, expected: str):
     episode.title = ''
     episode.file_type = test_value
-    episode.calculate_file_path('.')
+    episode.calculate_file_path(Path('.'))
     assert str(episode.path).endswith(expected)
 
 
-@pytest.mark.parametrize('test_value', ('audio/example',
-                                        'audio/random',
-                                        'video/mp4'))
+@pytest.mark.parametrize('test_value', (
+    'audio/example',
+    'audio/random',
+    'video/mp4',
+))
 def test_path_unknown_mimetype(episode: Episode, test_value: str):
     episode.title = ''
     episode.file_type = test_value
     with pytest.raises(EpisodeException):
-        episode.calculate_file_path('.')
+        episode.calculate_file_path(Path('.'))
--- podcastdownloader/tests/test_feed.py
+++ podcastdownloader/tests/test_feed.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 import pytest
+
 from podcastdownloader.feed import Feed
 
 
 @pytest.fixture
-def feed():
+def feed() -> Feed:
     return Feed('https://rss.art19.com/wecrashed')
 
 
-def test_fetchRSS(feed):
+def test_fetchRSS(feed: Feed):
     feed.fetch_rss()
     assert feed.title == 'WeCrashed: The Rise and Fall of WeWork'
 
 
-def test_extractEpisodes(feed):
+def test_extractEpisodes(feed: Feed):
     feed.fetch_rss()
     feed.extract_episodes(-1)
     assert len(feed.feed_episodes) == 10
