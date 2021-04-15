19eaa52fdb234569d54070f11349d9f8afd63053
Serene-Arc <serenical@gmail.com>
2021-04-15 16:07:24 +1000

    Skip test

--- podcastdownloader/tests/test_integration.py
+++ podcastdownloader/tests/test_integration.py
@@ -9,6 +9,7 @@ from click.testing import CliRunner
 from podcastdownloader.__main__ import cli
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('test_arguments', (
     ['-t', '1', '-l', '1', '-f', 'https://rss.art19.com/wecrashed'],
 ))
